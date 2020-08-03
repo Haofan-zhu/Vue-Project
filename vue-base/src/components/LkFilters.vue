04c2b03388e28513a806a23670c4ab83afec5751
Zhuhaofan <494019287@qq.com>
2020-08-03 13:29:52 +0800

    master

--- vue-base/src/components/LkFilters.vue
+++ vue-base/src/components/LkFilters.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
-    aaa
+    <h3>格式化人民币</h3>
+    <p>{{ money | moneyFormat }}</p>
+
+    <!-- 格式化日期 -->
+    <h1>格式化日期</h1>
+    <p>{{time | timeFormat}}</p>
+    <p>{{time | timeFormat('YYYY-MM-DD')}}</p>
+    <p>{{time | timeFormat('HH:mm:ss')}}</p>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
+import moment from 'moment'
 
+export default {
+  data () {
+    return {
+      money: 12345,
+      time: new Date(),
+    }
+  }, filters: {
+    moneyFormat (val) {
+      return '￥' + Number(val).toFixed(2);
+    },
+    timeFormat (val, format = "YYYY-MM-DD HH:mm:ss") {
+      return moment(val).format(format)
     }
   },
-  methods: {
-
+  mounted () {
+    setInterval(() => {
+      this.time = new Date()
+    }, 1000)
   },
-  components: {
-
-  }
 }
 </script>
 
 <style scoped>
-
 </style>
