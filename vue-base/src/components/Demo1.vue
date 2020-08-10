a4b352646556d92ab1be9cadd9126affc82681f1
Zhuhaofan <494019287@qq.com>
2020-08-10 16:05:16 +0800

    master

---
+++ vue-base/src/components/Demo1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{content}}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      content: "张三"
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
